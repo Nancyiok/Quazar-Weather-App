86b3b5558b14df106382d0f2c5379e1a6dfdf7f4
Anastasiia Kurdiukova <anastasiia.kurdiukova@cleveroad.com>
2026-03-18 21:06:51 +0200

    add not found page

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-//
 </script>
 
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-
+  <div class="fullscreen text-white text-center q-pa-md flex flex-center">
+    <p class="text-uppercase text-center text-h1">
+        404 <span class="text-uppercase text-accent text-h3">not found</span>
+      </p>
   </div>
 </template>
